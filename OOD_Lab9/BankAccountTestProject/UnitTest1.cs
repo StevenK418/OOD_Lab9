8f3c8203e10dad5bea3da7bd79d6039485c00b5f
StevenK418 <stevenkelly418@gmail.com>
2022-03-30 11:24:25 +0100

    => Added Testdeposit method. => Added TestMultipleDeposit Method. => Added TestZeroBalance method.

--- OOD_Lab9/BankAccountTestProject/UnitTest1.cs
+++ OOD_Lab9/BankAccountTestProject/UnitTest1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Security.Cryptography.X509Certificates;
 using BankAccountTestProject;
+using OOD_Lab9;
 
 namespace BankAccountTestProject
 {
@@ -8,8 +10,46 @@ namespace BankAccountTestProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestDeposit()
         {
+            //Arrange
+            BankAccount b1 = new BankAccount();
+            decimal expectedBalance = 200m;
+
+            //Act
+            b1.DepositAmount(200m);
+
+            //Assert
+            Assert.AreEqual(expectedBalance, b1.Balance);
+        }
+
+        [TestMethod]
+        public void TestMultipleDeposits()
+        {
+            //Arrange
+            BankAccount b1 = new BankAccount();
+            decimal expectedBalance = 200m;
+
+            //Act
+            b1.DepositAmount(200m);
+            b1.DepositAmount(60m);
+            b1.DepositAmount(40m);
+
+            //Assert
+            Assert.AreEqual(expectedBalance, b1.Balance);
+        }
+
+        [TestMethod]
+        public void TestNewAccountHasZeroBalance()
+        {
+            //Arrange
+            BankAccount b1 = new BankAccount();
+            decimal expectedBalance = 0m;
+
+            //Act
+            
+            //Assert
+            Assert.AreEqual(expectedBalance, b1.Balance);
         }
     }
 }
