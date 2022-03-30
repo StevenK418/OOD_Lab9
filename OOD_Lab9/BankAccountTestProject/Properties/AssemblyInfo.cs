3c659b3eb3c27b923d90adfab63671ef448fb69e
StevenK418 <stevenkelly418@gmail.com>
2022-03-30 10:36:11 +0100

    => Added basic project and BanAccount class. => Added test project.

---
+++ OOD_Lab9/BankAccountTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankAccountTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankAccountTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0c75285c-7381-4fac-a423-204a25840b90")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ OOD_Lab9/OOD_Lab9/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOD_Lab9
+{
+    public class BankAccount
+    {
+        public decimal Balance { get; set; }
+
+
+        /// <summary>
+        /// Deposits a given amount into the account balance.
+        /// </summary>
+        /// <param name="amount"></param>
+        public void DepositAmount(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        /// <summary>
+        /// Withdraws a given amount from the account Balance.
+        /// </summary>
+        /// <param name="amount"></param>
+        public void WithdrawAmount(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+
+
+    }
+}
