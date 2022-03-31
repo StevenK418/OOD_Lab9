90edee8caae2d884bde677a27ba857bc0354c536
StevenK418 <stevenkelly418@gmail.com>
2022-03-31 11:03:00 +0100

    => Added TestWithdrawSufficientFunds test method. => Added XML comments to all existing test methods.

--- OOD_Lab9/BankAccountTestProject/UnitTest1.cs
+++ OOD_Lab9/BankAccountTestProject/UnitTest1.cs
@@ -9,6 +9,10 @@ namespace BankAccountTestProject
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// Tests the BankAccount's deposit method and resultant balance
+        /// using a given amount.
+        /// </summary>
         [TestMethod]
         public void TestDeposit()
         {
@@ -23,6 +27,11 @@ namespace BankAccountTestProject
             Assert.AreEqual(expectedBalance, b1.Balance);
         }
 
+
+        /// <summary>
+        /// Tests the BankAccount's deposit method and resultant balance
+        /// using multiple amounts.
+        /// </summary>
         [TestMethod]
         public void TestMultipleDeposits()
         {
@@ -39,6 +48,10 @@ namespace BankAccountTestProject
             Assert.AreEqual(expectedBalance, b1.Balance);
         }
 
+        /// <summary>
+        /// Tests the BankAccount's balance is zero when
+        /// a new instance has been initialized. 
+        /// </summary>
         [TestMethod]
         public void TestNewAccountHasZeroBalance()
         {
@@ -48,6 +61,25 @@ namespace BankAccountTestProject
 
             //Act
             
+            //Assert
+            Assert.AreEqual(expectedBalance, b1.Balance);
+        }
+
+        /// <summary>
+        /// Tests the BankAccount withdrawal method when account
+        /// has sufficient funds for withdrawal amount. 
+        /// </summary>
+        [TestMethod]
+        public void TestWithdrawSufficientFunds()
+        {
+            //Arrange
+            BankAccount b1 = new BankAccount();
+            b1.DepositAmount(200m);
+            decimal expectedBalance = 100m;
+
+            //Act
+            b1.WithdrawAmount(100m);
+
             //Assert
             Assert.AreEqual(expectedBalance, b1.Balance);
         }
